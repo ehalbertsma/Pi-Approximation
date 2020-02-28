676ec5603ba0856fe0b920a18b40e926f3849006
ehalbertsma <59008238+ehalbertsma@users.noreply.github.com>
2020-02-28 12:14:04 -0500

    Create pi_approximator.py

---
+++ pi_approximator.py
@@ -0,0 +1,28 @@
+#-----------------------------
+# Pi Approximator
+# Emrys Halbertsma, 28/02/2020
+# Uses nothing but a uniform random number generator to approximate pi
+#-----------------------------
+
+# import statements
+import numpy.random as rd
+from numpy import zeros
+from numpy import sum
+
+# declare parameters
+n = 100
+points = 100000
+
+# declare storage variables
+circles = pis = zeros((1,n))
+
+for i in range(n): # independently approximate n number of pi_hats
+    circle = 0
+    for i in range(points): # number of darts to throw
+        a,b=rd.random(),rd.random() # throw point at the dartboard uniformly
+        if a**2+b**2<=1: # if the distance from the origin is greater than 1, it is by defn outside the circle
+            circle+=1 # counts the number of points inside the circle
+    pi_hat = 4*circle/points # number of points in the first quarter circle/number of points in the square gives pi_hat
+    pis[i] =  (pi_hat) # add pi_hat to the list of pi's
+
+print(sum(pis)/n) # average value of pi
